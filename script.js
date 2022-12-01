1bdf3677c036bc24e2a5fc0eb2769ede507c3e53
Makoto <115673367+IdioticGhost@users.noreply.github.com>
2022-12-01 02:33:29 +0000

    Finished the Q1, working on the Radio buttons.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', buzzfeedQuiz, false);
 function buzzfeedQuiz() {
 
   // task 4: declare variables for the html elements you'll use as selectors later. You need the submit button, display div, and all three inputs for your questions.
+  let coverPhoto = document.querySelector(".coverPhoto");
   let mustBeThree = document.querySelector(".hastobethree")
   let button = document.querySelector(".submit");
   let displayResult = document.querySelector(".displayResult");
@@ -30,14 +31,17 @@ let secondFavoriteNumber = Number(q2.value);
 let thirdFavoriteNumber = Number(q3.value);
 let sumFavoriteNumber = q4;
 
+let yes = document.querySelector("input[name='yes']:checked");
+    yes.value
     // task 7: create a conditional statement for the responses to Question 1 based on the user input.
 
 if (firstFavoriteNumber !== 3) {
-mustBeThree.innerHTML('beforeend',"incorrect")
-mustBeThree.style = "color: #eb2642";
+mustBeThree.innerHTML = "disappointing."
+mustBeThree.style = "color: FF3131";
 } else {
-  mustBeThree.innerHTML('beforeend',"correct!")
-  mustBeThree.style = "color: #cbd0bd";
+  mustBeThree.innerHTML = "correct!"
+  mustBeThree.style = "color: #32CD32";
+  coverPhoto.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAcMynvvO_skZX5v5LwlTUD0DFV9bA3HLYhoPUMYqjBNWUaTsqGq9LuJfOJOfkLGsXs-c&usqp=CAU"
 }
 
 
@@ -60,7 +64,6 @@ if (sumFavoriteNumber === 1) {
 
     // task 10: declare a variable for the total score of the quiz and set it equal to the sum of each question score.
 
-l
 
 
     // task 11: create a conditional statement for the result to the quiz based on the total score. Using string interpolation, display these to the page: the result, an image that corresponds and a message to go with it.
